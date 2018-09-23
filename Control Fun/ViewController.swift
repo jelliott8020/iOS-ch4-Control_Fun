13ebe278c8379dde73e3dc2d13f77e37862bbb68
Josh Elliott <josh@joshs-macbook-pro.local>
2018-09-23 16:23:35 -0400

    Initial Commit

---
+++ Control Fun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Control Fun
+//
+//  Created by Josh Elliott on 9/23/18.
+//  Copyright © 2018 JoshElliott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
