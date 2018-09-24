959a4b5029c82d7ac859c96450c2f979e05798b9
Josh Elliott <josh@2600-6c5e-6680-03d2-0000-bb83-ddaa-f783.dhcp6.chtrptr.net>
2018-09-24 06:47:11 -0400

    Finished Basic Chapter

--- Control Fun/ViewController.swift
+++ Control Fun/ViewController.swift
@@ -48,10 +48,42 @@ class ViewController: UIViewController {
     }
     
     @IBAction func onButtonPressed(_ sender: UIButton) {
+        let controller = UIAlertController(title: "Are You Sure?",
+                                           message:nil, preferredStyle: .actionSheet)
+        
+        let yesAction = UIAlertAction(title: "Yes, I'm sure!",
+                                      style: .destructive, handler: { action in
+                                        let msg = self.nameField.text!.isEmpty
+                                            ? "You can breathe easy, everything went OK."
+                                            : "You can breathe easy, \(self.nameField.text),"
+                                            + "everything went OK."
+                                        let controller2 = UIAlertController(
+                                            title:"Something Was Done",
+                                            message: msg, preferredStyle: .alert)
+                                        let cancelAction = UIAlertAction(title: "Phew!",
+                                                                         style: .cancel,
+                                                                         handler: nil)
+                                        controller2.addAction(cancelAction)
+                                        self.present(controller2, animated: true,
+                                                     completion: nil)
+        })
+        
+        let noAction = UIAlertAction(title: "No Way!",
+                                     style: .cancel, handler: nil)
+        
+        controller.addAction(yesAction)
+        controller.addAction(noAction)
+        
+        if let ppc = controller.popoverPresentationController {
+            ppc.sourceView = sender
+            ppc.sourceRect = sender.bounds
+        }
+        
+        present(controller, animated: true, completion: nil)
     }
     
     @IBAction func toggleControls(_ sender: UISegmentedControl) {
-        if sender.selectedSegmentIndex == 0 {
+        if sender.selectedSegmentIndex == 0 {  // "Switches" is selected
             leftSwitch.isHidden = false
             rightSwitch.isHidden = false
             doSomethingButton.isHidden = true
